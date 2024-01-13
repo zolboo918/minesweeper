84de80338bbaa3416fe059b5f788bfa644341809
zolboo918 <50549974+zolboo918@users.noreply.github.com>
2024-01-14 02:30:49 +0800

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,353 @@
-import Image from 'next/image'
+"use client";
+
+import { useState } from "react";
+import Image from "next/image";
+
+type Level = "easy" | "medium" | "hard";
+
+type CellValueType = {
+  type: "empty" | "bomb" | "flag" | "opened";
+  aroundBombCount: number;
+};
+
+const LevelRowColCount = {
+  easy: 10,
+  medium: 18,
+  hard: 28,
+};
+
+const LevelBombCount = {
+  easy: 10,
+  medium: 40,
+  hard: 70,
+};
+
+let SelectedLevel: Level = "easy";
+let clickCount = 0;
+let BombCount = 0;
 
 export default function Home() {
+  const [started, setStarted] = useState<boolean>(false);
+  const [level, setLevel] = useState<Level>("easy");
+  const [array, setArray] = useState<CellValueType[][] | [][]>([[], []]);
+  const [result, setResult] = useState<string | null>();
+
+  const cellSize =
+    level === "easy"
+      ? "h-[30px] w-[30px]"
+      : level === "medium"
+      ? "h-[25px] w-[25px]"
+      : "h-[20px] w-[20px]";
+
+  const chooseLevel = (selectedLevel: Level) => {
+    setLevel(selectedLevel);
+    SelectedLevel = selectedLevel;
+    setArray(generateArray(selectedLevel));
+    setStarted(true);
+  };
+
+  const generateArray = (level: Level) => {
+    let newArray: CellValueType[][] = [];
+    [...Array(LevelRowColCount[level])].forEach(() => {
+      let row: CellValueType[] = [];
+      [...Array(LevelRowColCount[level])].forEach(() => {
+        row.push({
+          aroundBombCount: 0,
+          type: "empty",
+        });
+      });
+      newArray.push(row);
+    });
+    return fillBomb(newArray, level);
+  };
+
+  const fillBomb = (array: CellValueType[][], level: Level) => {
+    [...Array(LevelBombCount[level])].forEach(() => {
+      const row = Math.floor(Math.random() * LevelRowColCount[level]);
+      const col = Math.floor(Math.random() * LevelRowColCount[level]);
+      if (
+        typeof array[row][col] != "undefined" &&
+        array[row][col].type === "empty"
+      ) {
+        array[row][col].type = "bomb";
+        BombCount++;
+      }
+    });
+    return array;
+  };
+
+  const checkCell = (rowIndex: number, colIndex: number) => {
+    return array[rowIndex] && array[rowIndex][colIndex];
+  };
+
+  const countAroundBomb = (
+    rowIndex: number,
+    colIndex: number,
+    newArr: CellValueType[][]
+  ) => {
+    // top
+    if (
+      checkCell(rowIndex - 1, colIndex) &&
+      newArr[rowIndex - 1][colIndex].type === "bomb"
+    ) {
+      newArr[rowIndex][colIndex].aroundBombCount++;
+    }
+    //bottom
+    if (
+      checkCell(rowIndex + 1, colIndex) &&
+      newArr[rowIndex + 1][colIndex].type === "bomb"
+    ) {
+      newArr[rowIndex][colIndex].aroundBombCount++;
+    }
+    //left
+    if (
+      checkCell(rowIndex, colIndex - 1) &&
+      newArr[rowIndex][colIndex - 1].type === "bomb"
+    ) {
+      newArr[rowIndex][colIndex].aroundBombCount++;
+    }
+    //right
+    if (
+      checkCell(rowIndex, colIndex + 1) &&
+      newArr[rowIndex][colIndex + 1].type === "bomb"
+    ) {
+      newArr[rowIndex][colIndex].aroundBombCount++;
+    }
+    // top left
+    if (
+      checkCell(rowIndex - 1, colIndex - 1) &&
+      newArr[rowIndex - 1][colIndex - 1].type === "bomb"
+    ) {
+      newArr[rowIndex][colIndex].aroundBombCount++;
+    }
+    // top right
+    if (
+      checkCell(rowIndex - 1, colIndex + 1) &&
+      newArr[rowIndex - 1][colIndex + 1].type === "bomb"
+    ) {
+      newArr[rowIndex][colIndex].aroundBombCount++;
+    }
+    //bottom left
+    if (
+      checkCell(rowIndex + 1, colIndex - 1) &&
+      newArr[rowIndex + 1][colIndex - 1].type === "bomb"
+    ) {
+      newArr[rowIndex][colIndex].aroundBombCount++;
+    }
+    //bottom right
+    if (
+      checkCell(rowIndex + 1, colIndex + 1) &&
+      newArr[rowIndex + 1][colIndex + 1].type === "bomb"
+    ) {
+      newArr[rowIndex][colIndex].aroundBombCount++;
+    }
+    return newArr;
+  };
+
+  const checkAround = (
+    rowIndex: number,
+    colIndex: number,
+    fromRecursive?: boolean,
+    arr?: CellValueType[][]
+  ) => {
+    let newArr: CellValueType[][] = arr ? arr : [...array];
+    if (newArr[rowIndex] && newArr[rowIndex][colIndex] && result != "0") {
+      const value = newArr[rowIndex][colIndex];
+
+      if (!fromRecursive) {
+        clickCount++;
+      }
+
+      if (value.type === "bomb" && !fromRecursive) {
+        if (clickCount === 1) {
+          chooseLevel(SelectedLevel);
+          return;
+        } else {
+          setResult("0");
+          return;
+        }
+      }
+
+      if (value.type === "flag") {
+        return;
+      }
+
+      if (value.type === "empty") {
+        newArr[rowIndex][colIndex].type = "opened";
+        if (!fromRecursive && checkWin()) {
+          alert("You win! Congrats.");
+          reset();
+          return;
+        }
+        newArr = countAroundBomb(rowIndex, colIndex, newArr);
+        if (newArr[rowIndex][colIndex].aroundBombCount === 0) {
+          // top
+          if (checkCell(rowIndex - 1, colIndex)) {
+            checkAround(rowIndex - 1, colIndex, true);
+          }
+          //bottom
+          if (checkCell(rowIndex + 1, colIndex)) {
+            checkAround(rowIndex + 1, colIndex, true);
+          }
+          //left
+          if (checkCell(rowIndex, colIndex - 1)) {
+            checkAround(rowIndex, colIndex - 1, true);
+          }
+          //right
+          if (checkCell(rowIndex, colIndex + 1)) {
+            checkAround(rowIndex, colIndex + 1, true);
+          }
+          // top left
+          if (checkCell(rowIndex - 1, colIndex - 1)) {
+            checkAround(rowIndex - 1, colIndex - 1, true);
+          }
+          // top right
+          if (checkCell(rowIndex - 1, colIndex + 1)) {
+            checkAround(rowIndex - 1, colIndex + 1, true);
+          }
+          //bottom left
+          if (checkCell(rowIndex + 1, colIndex - 1)) {
+            checkAround(rowIndex + 1, colIndex - 1, true);
+          }
+          //bottom right
+          if (checkCell(rowIndex + 1, colIndex + 1)) {
+            checkAround(rowIndex + 1, colIndex + 1, true);
+          }
+        }
+      }
+      setArray([...newArr]);
+    }
+  };
+
+  const setFlag = (event: MouseEvent, rowIndex: number, colIndex: number) => {
+    event.preventDefault();
+
+    if (array[rowIndex][colIndex].type == "flag") {
+      array[rowIndex][colIndex].type = "empty";
+    } else if (
+      array[rowIndex][colIndex].type !== "flag" &&
+      array[rowIndex][colIndex].type !== "opened"
+    ) {
+      array[rowIndex][colIndex].type = "flag";
+    }
+    setArray([...array]);
+  };
+
+  const reset = () => {
+    setResult(null);
+    setStarted(false);
+    clickCount = 0;
+    BombCount = 0;
+  };
+
+  const checkWin = () => {
+    let openedCellCount = 0;
+    array.forEach((row: CellValueType[]) => {
+      row.forEach((col: CellValueType) => {
+        if (col.type === "opened") {
+          openedCellCount++;
+        }
+      });
+    });
+    return openedCellCount === LevelRowColCount[SelectedLevel] ** 2 - BombCount;
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="flex min-h-screen flex-col items-center justify-center p-12">
+      {!started ? (
+        <div>
+          <div className="flex self-center text-center mb-5">Choose level</div>
+          <div>
+            <button
+              className="rounded-lg bg-green-100 text-gray-600 px-5 py-2 mx-2"
+              onClick={() => chooseLevel("easy")}
+            >
+              Easy
+            </button>
+            <button
+              className="rounded-lg bg-green-100 text-gray-600 px-5 py-2 mx-2"
+              onClick={() => chooseLevel("medium")}
+            >
+              Medium
+            </button>
+            <button
+              className="rounded-lg bg-green-100 text-gray-600 px-5 py-2 mx-2"
+              onClick={() => chooseLevel("hard")}
+            >
+              Hard
+            </button>
+          </div>
         </div>
-      </div>
+      ) : (
+        <div>
+          <button className="text-2xl mr-10" onClick={reset}>
+            &lt;-{" "}
+          </button>
+          <span>{level.toUpperCase()}</span>
+          <div className="mt-5">
+            {array.map((row: CellValueType[], rowIndex: number) => {
+              return (
+                <div className="flex row" key={rowIndex}>
+                  {row.map((column: CellValueType, columnIndex: number) => {
+                    return (
+                      <div
+                        key={rowIndex + columnIndex}
+                        className={`flex ${
+                          column.type === "opened" ? "bg-green-200" : "bg-white"
+                        } text-gray-400 justify-center align-middle rounded hover:bg-green-200 text-center text-xl m-[1px] ${cellSize}`}
+                        onClick={() => checkAround(rowIndex, columnIndex)}
+                        onContextMenu={(event: any) =>
+                          setFlag(event, rowIndex, columnIndex)
+                        }
+                      >
+                        {column.aroundBombCount > 0 ? (
+                          column.aroundBombCount
+                        ) : column.type === "flag" ? (
+                          <Image
+                            src={
+                              "https://cdn-icons-png.flaticon.com/512/395/395841.png"
+                            }
+                            alt="flag"
+                            width={10}
+                            height={5}
+                            className="h-[70%] w-[70%]"
+                          />
+                        ) : result === "0" && column.type == "bomb" ? (
+                          <Image
+                            src={
+                              "https://static.vecteezy.com/system/resources/thumbnails/021/125/685/small/bomb-flat-icon-png.png"
+                            }
+                            alt="bomb"
+                            width={10}
+                            height={5}
+                            className="h-[70%] w-[70%]"
+                          />
+                        ) : (
+                          <></>
+                        )}
+                      </div>
+                    );
+                  })}
+                </div>
+              );
+            })}
+          </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+          {result === "0" && (
+            <div className="text-l mt-10 ">
+              <div>
+                <span>Game over</span>
+              </div>
+              <button
+                className="px-5 py-2 rounded-lg bg-cyan-600 mt-5"
+                onClick={reset}
+              >
+                Play again
+              </button>
+            </div>
+          )}
+        </div>
+      )}
     </main>
-  )
+  );
 }
